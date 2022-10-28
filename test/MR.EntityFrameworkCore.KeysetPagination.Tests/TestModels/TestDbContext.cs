810a383f8ff2ca10b8746961f05537a68b3eb126
Mohammad Rahhal <m.r992@hotmail.com>
2022-10-28 10:05:48 +0200

    Store log messages in test

--- test/MR.EntityFrameworkCore.KeysetPagination.Tests/TestModels/TestDbContext.cs
+++ test/MR.EntityFrameworkCore.KeysetPagination.Tests/TestModels/TestDbContext.cs
@@ -4,16 +4,31 @@ namespace MR.EntityFrameworkCore.KeysetPagination.TestModels;
 
 public class TestDbContext : DbContext
 {
+	private readonly List<string> _logMessages = new();
+
 	public TestDbContext(
 		DbContextOptions<TestDbContext> options)
 		: base(options)
 	{
 	}
 
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.LogTo(message =>
+		{
+			lock (_logMessages)
+			{
+				_logMessages.Add(message);
+			}
+		});
+	}
+
 	protected override void OnModelCreating(ModelBuilder builder)
 	{
 		base.OnModelCreating(builder);
 	}
+
+	public IEnumerable<string> LogMessages => _logMessages;
 
 	public DbSet<IntModel> IntModels { get; set; }
 
