168c2f6c582d1efb6fe1a06be50113c12b70b401
Mohammad Rahhal <m.r992@hotmail.com>
2023-05-05 15:14:27 +0200

    Add benchmark

---
+++ benchmarks/Benchmarks.Basic/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class BenchmarkDbContext : DbContext
+{
+	public BenchmarkDbContext(DbContextOptions options) : base(options)
+	{
+	}
+
+	public DbSet<User> Users { get; set; } = default!;
+}
+
+[Index(nameof(Created), nameof(Id))]
+public class User
+{
+	public int Id { get; set; }
+
+	public string Name { get; set; } = default!;
+
+	public DateTime Created { get; set; }
+}
